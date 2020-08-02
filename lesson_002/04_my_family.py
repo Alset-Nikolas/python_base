ba3fcbd182eae0971fc020f797961538c8408327
Николай Хватов <Alset.Nikolas@yandex.ru>
2020-08-02 22:46:00 +0300

    Исправил задание №5

--- lesson_002/04_my_family.py
+++ lesson_002/04_my_family.py
@@ -20,5 +20,5 @@ my_family_height = [
 print("Рост отца -", my_family_height[1][1], 'см')
 # Выведите на консоль общий рост вашей семьи как сумму ростов всех членов
 print("Общий рост моей семьи -", my_family_height[0][1] +
-my_family_height[1][1] + my_family_height[2][1], 'см')  
+my_family_height[1][1] + my_family_height[2][1], 'см')
 # Общий рост моей семьи - ХХ см
