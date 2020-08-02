8125c198f3dcfb2e30d1a3fce913e45a50caf0c6
Николай Хватов <Alset.Nikolas@yandex.ru>
2020-08-02 12:41:46 +0300

    Задание №1: Словарь словарей расстояний между городами

--- lesson_002/00_distance.py
+++ lesson_002/00_distance.py
@@ -1,19 +1,36 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-
+from pprint import pprint
 # Есть словарь координат городов
 
 sites = {
-    'Moscow': (550, 370),
-    'London': (510, 510),
-    'Paris': (480, 480),
+'Moscow': (550, 370),
+'London': (510, 510),
+'Paris': (480, 480),
 }
 
 # Составим словарь словарей расстояний между ними
 # расстояние на координатной сетке - ((x1 - x2) ** 2 + (y1 - y2) ** 2) ** 0.5
 
+Moscow = sites["Moscow"]
+London = sites["London"]
+Paris = sites["Paris"]
+
+Moscow_London = ((Moscow[0] - London[0]) ** 2 + (Moscow[1] - London[1]) ** 2) ** 0.5
+Moscow_Paris = ((Moscow[0] - Paris[0]) ** 2 + (Moscow[1] -Paris[1]) ** 2) ** 0.5
+London_Paris = ((London[0] - Paris[0]) ** 2 + (London[1] -Paris[1]) ** 2) ** 0.5
+
 distances = {}
+distances['Moscow'] = {}
+distances['Moscow']['London'] = Moscow_London
+distances['Moscow']['Paris'] = Moscow_Paris
 
-# TODO здесь заполнение словаря
+distances['London'] = {}
+distances['London']['Moscow'] = Moscow_London
+distances['London']['Paris'] = London_Paris
 
-print(distances)
+distances['Paris'] = {}
+distances['Paris']['Moscow'] = Moscow_Paris
+distances['Paris']['London'] = London_Paris
+
+pprint(distances)
