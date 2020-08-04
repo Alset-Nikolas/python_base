dbacd07124731c84d3ebc38c1a350e960644bad1
Николай Хватов <Alset.Nikolas@yandex.ru>
2020-08-04 13:37:05 +0300

    Задание №6: 7 линий разного цвета

--- lesson_003/06_rainbow.py
+++ lesson_003/06_rainbow.py
@@ -8,13 +8,35 @@ rainbow_colors = (sd.COLOR_RED, sd.COLOR_ORANGE, sd.COLOR_YELLOW, sd.COLOR_GREEN
                   sd.COLOR_CYAN, sd.COLOR_BLUE, sd.COLOR_PURPLE)
 
 # Нарисовать радугу: 7 линий разного цвета толщиной 4 с шагом 5 из точки (50, 50) в точку (350, 450)
-# TODO здесь ваш код
+
+
+
+start_x, start_y = 50, 50
+finish_x, finish_y = 350, 450
+for color in rainbow_colors:
+    point_1 = sd.Point(start_x, start_y)
+    point_2 = sd.Point(finish_x, finish_y)
+    point_list = [point_1, point_2]
+    sd.lines(point_list, color=color, closed=False, width=4)
+    start_x += 5
+    finish_x += 5
+
+
+
+
 # Подсказка: цикл нужно делать сразу по тьюплу с цветами радуги.
 
 
 # Усложненное задание, делать по желанию.
 # Нарисовать радугу дугами от окружности (cсм sd.circle) за нижним краем экрана,
 # поэкспериментировать с параметрами, что бы было красиво
-# TODO здесь ваш код
 
+radius = 550
+center_position = sd.Point(600, 0)
+
+for color in rainbow_colors:
+    sd.circle(center_position, radius=radius, color=color, width=10)
+    radius +=10
 sd.pause()
+
+
