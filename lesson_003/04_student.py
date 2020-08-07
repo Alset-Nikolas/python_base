eeb06784a41e7d0a9e742cc3209359ca0706ff5f
Николай Хватов <Alset.Nikolas@yandex.ru>
2020-08-07 12:10:24 +0300

    Задание №4: Исправил

--- lesson_003/04_student.py
+++ lesson_003/04_student.py
@@ -15,25 +15,26 @@ percents_change = 3 / 100
 
 scholarship_for_10_month = 10 * educational_grant
 expenses_for_i_month = 0
-for _ in range(9):
+for _ in range(10):
     expenses_for_i_month += expenses
     expenses *= 1 + percents_change
 else:
     print('Студенту надо попросить', round(expenses_for_i_month - scholarship_for_10_month, 2), 'рублей')
 
-'''  
+#----------------------------------------------------------------------------------------------------------------------
+
 educational_grant, expenses = 10000, 12000
 percents_change = 3 / 100
 summa = expenses - educational_grant
 
-for _ in range(8):
+for _ in range(9):
     expenses *= 1 + percents_change
     summa += expenses - educational_grant
 
 else:
     print('Студенту надо попросить', round(summa, 2), 'рублей')
 
-Слегка запутался, не могу понять почему ответ другой...? 
 
-'''
+
+
 
