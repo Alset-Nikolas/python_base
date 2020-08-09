ac5129f33812db174694cef8d8e13cbe8e19e30b
Николай Хватов <Alset.Nikolas@yandex.ru>
2020-08-09 12:25:50 +0300

    Задание №4: Cнегопад

--- lesson_004/05_snowfall.py
+++ lesson_004/05_snowfall.py
@@ -2,29 +2,66 @@
 
 import simple_draw as sd
 
-
 # На основе кода из практической части реализовать снегопад:
 # - создать списки данных для отрисовки N снежинок
 # - нарисовать падение этих N снежинок
 # - создать список рандомных длин лучей снежинок (от 10 до 100) и пусть все снежинки будут разные
-
+sd.resolution = (700, 700)
 N = 20
 
 # Пригодятся функции
-# sd.get_point()
-# sd.snowflake()
-# sd.sleep()
+# sd.get_psd.snowflake()
+# # sd.sleep()oint()
+#
 # sd.random_number()
 # sd.user_want_exit()
 
+sd.start_drawing()
+sd.finish_drawing()
+
+start_x = [0] * N
+start_y = [0] * N
+snowfall_length = [0] * N
+start_point = [0] * N
+flag_add_new_snowfall = [0] * N
+for i in range(N):
+    start_x[i] = sd.random_number(100, 600)
+    start_y[i] = sd.random_number(100, 600)
+    snowfall_length[i] = sd.random_number(10, 30)
+    start_point[i] = sd.Point(start_x[i], start_y[i])
+
+
+def add_snowfall():
+    start_x.append(sd.random_number(100, 600))
+    start_y.append(sd.random_number(100, 600))
+    snowfall_length.append(sd.random_number(10, 30))
+    start_point.append(sd.Point(start_x[i], start_y[i]))
+    flag_add_new_snowfall.append(0)
+
 
 while True:
-    sd.clear_screen()
-    # TODO здесь ваш код
+    sd.start_drawing()
+
+    for i in range(N):
+        sd.snowflake(center=start_point[i], length=snowfall_length[i], color=sd.background_color)
+        if start_y[i] - snowfall_length[i] <= 0 and flag_add_new_snowfall[i] == 0:
+            add_snowfall()
+            N += 1
+            flag_add_new_snowfall[i] = 1
+        elif start_y[i] - snowfall_length[i] <= 0 and flag_add_new_snowfall[i] == 1:
+            start_y[i] -= 0
+            start_x[i] -= 0
+        else:
+            start_y[i] -= sd.random_number(1, 5)
+            start_x[i] -= sd.random_number(-2, 2)
+        start_point[i] = sd.Point(start_x[i], start_y[i])
+        sd.snowflake(center=start_point[i], length=snowfall_length[i], color=sd.COLOR_WHITE)
+
     sd.sleep(0.1)
+
     if sd.user_want_exit():
         break
-sd.pause()
+    sd.finish_drawing()
 
 # Примерный алгоритм отрисовки снежинок
 #   навсегда
