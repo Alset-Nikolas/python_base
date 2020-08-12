d14a58b4d595aea9abef8d76159ccf558e130c12
Николай Хватов <Alset.Nikolas@yandex.ru>
2020-08-12 21:09:53 +0300

    Задание №2: Вывести на консоль район

--- lesson_005/02_district.py
+++ lesson_005/02_district.py
@@ -5,4 +5,12 @@
 # подсказка: для вывода элементов списка через запятую можно использовать функцию строки .join()
 # https://docs.python.org/3/library/stdtypes.html#str.join
 
-# TODO здесь ваш код
+from lesson_005.district.central_street.house1.room1 import folks as people_central_street_house1_room1
+from lesson_005.district.central_street.house1.room2 import folks as people_central_street_house1_room2
+from lesson_005.district.soviet_street.house2.room1 import folks as people_central_street_house2_room1
+from lesson_005.district.soviet_street.house2.room2 import folks as people_central_street_house2_room2
+
+people = people_central_street_house1_room1 + people_central_street_house1_room2 \
+         + people_central_street_house2_room1+people_central_street_house2_room2
+
+print('На районе живут: ', ', '.join(people))
