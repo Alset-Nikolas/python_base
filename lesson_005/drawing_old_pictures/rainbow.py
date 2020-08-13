80319b698dcc5c27ff0c7e91023e16ac94caebe5
Николай Хватов <Alset.Nikolas@yandex.ru>
2020-08-13 09:46:32 +0300

    Задание №4: rainbow

--- lesson_005/drawing_old_pictures/rainbow.py
+++ lesson_005/drawing_old_pictures/rainbow.py
@@ -1,12 +1,11 @@
 # -*- coding: utf-8 -*-
-def rainbow_picture():
+def rainbow_picture(x_center=600, y_center=0, radius=500):
     import simple_draw as sd
 
     rainbow_colors = (sd.COLOR_RED, sd.COLOR_ORANGE, sd.COLOR_YELLOW, sd.COLOR_GREEN,
                       sd.COLOR_CYAN, sd.COLOR_BLUE, sd.COLOR_PURPLE)
 
-    radius = 550
-    center_position = sd.Point(600, 0)
+    center_position = sd.Point(x_center, y_center)
 
     for color in rainbow_colors[::-1]:
         sd.circle(center_position, radius=radius, color=color, width=10)
@@ -15,4 +14,4 @@ def rainbow_picture():
 
 
 if __name__ == '__main__':
-    rainbow_picture()
+    rainbow_picture(x_center=600, y_center=0, radius=500)
