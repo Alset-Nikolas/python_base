536ac07e95c78ee066cffbfbe76b9bcdf0544c48
Николай Хватов <Alset.Nikolas@yandex.ru>
2020-08-14 13:04:04 +0300

    pep8

--- lesson_005/01_inhabitants.py
+++ lesson_005/01_inhabitants.py
@@ -6,8 +6,5 @@
 from room_1 import folks as people_room_1
 from room_2 import folks as people_room_2
 
-#print('В комнате room_1 живут:', people_room_1[0], 'и', people_room_1[1])
 print('В комнате room_1 живут:', ', '.join(people_room_1))
 print('В комнате room_2 живут:', ', '.join(people_room_2))
-#print('В комнате room_2 живет:', people_room_2[0])
-
