1f2f9e97a4c4c590bb91375790ab73e037b34af5
Николай Хватов <Alset.Nikolas@yandex.ru>
2020-08-14 13:02:04 +0300

    Исправил 4

--- lesson_005/04_painting.py
+++ lesson_005/04_painting.py
@@ -18,30 +18,17 @@
 # пример см. lesson_005/results/04_painting.jpg
 # Приправить своей фантазией по вкусу (коты? коровы? люди? трактор? что придумается)
 import simple_draw as sd
-from lesson_005.drawing_old_pictures.house import hose_draw
-from lesson_005.animation_painting.snowfall import snowfall_draw
-from lesson_005.drawing_old_pictures.rainbow import rainbow_picture
-from lesson_005.drawing_old_pictures.street import street_draw
-from lesson_005.drawing_old_pictures.tree import tree_draw
-from lesson_005.animation_painting.sun import sun_draw
-from lesson_005.animation_painting.smile import smile_draw
-# TODO lesson_005. указывать не нужно
+from drawing_old_pictures.house import hose_draw
+from animation_painting.snowfall import snowfall_draw
+from drawing_old_pictures.rainbow import rainbow_picture
+from drawing_old_pictures.street import street_draw
+from drawing_old_pictures.tree import tree_draw
+from animation_painting.sun import sun_draw
+from animation_painting.smile import smile_draw
 
-# TODO лишний закомментированный код перед сдачей нужно убирать
-'''
-X_SIZE = 1200
-Y_SIZE = 800
-sd.resolution = (X_SIZE, Y_SIZE)
 
-hose_draw(x_left_down=X_SIZE // 2 - 200, y_left_down=200, x_size=400, y_size=300)
-rainbow_picture(x_center=420, y_center=150, radius=800)
-street_draw(X_SIZE, 200)
-sun_draw(100,Y_SIZE-60, 30)
-tree_draw(x_start=X_SIZE // 2 + 400, y_start=200, length=100)
-snowfall_draw(x_coordinates_down=0, y_coordinates_down=200,
-             x_coordinates_up=X_SIZE // 2 - 200-50, y_coordinates_up=Y_SIZE-200)
-sd.pause()
-'''
+
+
 # Усложненное задание (делать по желанию)
 # Анимировать картину.
 # Пусть слева идет снегопад, радуга переливается цветами, смайлик моргает, солнце крутит лучами, етс.
@@ -56,7 +43,7 @@ sd.resolution = (X_SIZE, Y_SIZE)
 snowfall_x, snowfall_y, snowfall_length, flag_add_new_snowfall, N = [], [], [], [], 20
 
 first_color = 0
-delta_for_sun =  0
+delta_for_sun = 0
 x_start_for_kolobok = 20
 
 hose_draw(x_left_down=X_SIZE // 2 - 200, y_left_down=200, x_size=400, y_size=300)
@@ -64,8 +51,9 @@ tree_draw(x_start=X_SIZE // 2 + 400, y_start=200, length=100)
 
 while True:
 
-    rainbow_picture(x_center=420, y_center=150, radius=800, first_color=first_color);first_color = (first_color + 1) % 7
+    rainbow_picture(x_center=420, y_center=150, radius=800, first_color=first_color)
     first_color = (first_color + 1) % 7
+
 
     snowfall_x, snowfall_y, snowfall_length, flag_add_new_snowfall, N = \
         snowfall_draw(x_coordinates_down=0,
@@ -81,9 +69,10 @@ while True:
     delta_for_sun = sun_draw(100, Y_SIZE - 60, 30, delta_for_sun)
 
     street_draw(X_SIZE, 200)
-    smile_draw(x_strart=x_start_for_kolobok, y_start=150); x_start_for_kolobok += 5
+    smile_draw(x_strart=x_start_for_kolobok, y_start=150)
+    x_start_for_kolobok += 5
 
-
+    print(locals())
     sd.sleep(0.3)
     if sd.user_want_exit():
         break
