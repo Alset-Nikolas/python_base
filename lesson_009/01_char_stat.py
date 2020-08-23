f998d9baf1c65861e7b01ac094f1bf487a6b91ed
Николай Хватов <Alset.Nikolas@yandex.ru>
2020-08-23 10:51:22 +0300

    Модуль 9 на функциях

--- lesson_009/01_char_stat.py
+++ lesson_009/01_char_stat.py
@@ -25,8 +25,63 @@
 #   см https://refactoring.guru/ru/design-patterns/template-method
 #   и https://gitlab.skillbox.ru/vadim_shandrinov/python_base_snippets/snippets/4
 
-# TODO здесь ваш код
+import zipfile
+from pprint import pprint
 
+
+def unpacking_zip_file(zip_name):
+    ''' voina-i-mur.zip ---> voina-i-mur.txt'''
+
+    zfile = zipfile.ZipFile(zip_name, 'r')
+    txt_name_file = zfile.namelist()[0]
+    for filename in zfile.namelist():
+        zfile.extract(filename)
+    print(f'Распаковка файла завершена! Появился файл "{txt_name_file}"')
+    return txt_name_file
+
+
+zip_name = 'C:\\Users\\User\\PycharmProjects\\python_base\\lesson_009\\python_snippets\\voyna-i-mir.txt.zip'
+txt_name_file = unpacking_zip_file(zip_name=zip_name)
+
+def stat_alfavit():
+    alfavit = {}
+
+    for x in range(ord('А'), ord('Я') + 1):
+        alfavit[chr(x)] = 0
+    for x in range(ord('а'), ord('я') + 1):
+        alfavit[chr(x)] = 0
+
+
+    with open(txt_name_file, 'r', encoding='cp1251') as file:
+        for line in file:
+            for symbol in line:
+                if 'а' <= symbol <= 'я' or 'А' <= symbol <= 'Я':
+                    alfavit[symbol] += 1
+
+    return alfavit
+
+alfavit = stat_alfavit()
+
+def sort_stat(alfavit):
+    stat = []
+    for key, value in alfavit.items():
+        stat.append((key, value))
+
+    for _ in range(len(stat)):
+        for x in range(len(stat)-1):
+            if stat[x][1] < stat[x+1][1]:
+                stat[x+1], stat[x] = stat[x], stat[x+1]
+    return stat
+stat = sort_stat(alfavit)
+
+
+def p_print(stat):
+    for letter, number in stat:
+        print('+---------+----------+')
+        print('|{:^9}'.format(letter), '|{:^10}|'.format(number), sep='')
+    else:
+        print('+---------+----------+')
+p_print(stat)
 # После зачета первого этапа нужно сделать упорядочивание статистики
 #  - по частоте по возрастанию
 #  - по алфавиту по возрастанию
