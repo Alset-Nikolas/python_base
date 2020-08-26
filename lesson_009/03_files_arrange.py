b59e49c38e873909d0ce72ce2d30bd9280e86c5e
Николай Хватов <Alset.Nikolas@yandex.ru>
2020-08-26 12:57:33 +0300

    Исправил

--- lesson_009/03_files_arrange.py
+++ lesson_009/03_files_arrange.py
@@ -47,11 +47,11 @@ import os.path
 class FilesArrange:
     def __init__(self, zip_name, path):
         self.zip_name = zip_name
-        self.path = path
+        self.path_unpack_dir = path
         self.data = []
 
     def unpacking_zip_file(self):
-        if not os.path.exists(self.path):
+        if not os.path.exists(self.path_unpack_dir):
             self.zfile = zipfile.ZipFile(self.zip_name, 'r')
             self.txt_name_file = self.zfile.namelist()[0]
             for filename in self.zfile.namelist():
@@ -60,72 +60,53 @@ class FilesArrange:
         else:
             self.zfile = zipfile.ZipFile(self.zip_name, 'r')
 
-    def normpath(self):
-        os.path.normpath(self.path)
+        self.path_unpack_dir = self.normpath(self.path_unpack_dir)
 
-    def walk_in_file(self):
-        for dirpath, dirnames, filenames in os.walk(self.path):
+    def normpath(self, path):
+        return os.path.normpath(path)
+
+    def walk_in_old_dir(self):
+        for dirpath, dirnames, filenames in os.walk(self.path_unpack_dir):
             if filenames:
-                for foto in filenames:
-                    all_dir_path = os.path.join(dirpath, foto)
+                for name_foto in filenames:
+                    all_dir_path = os.path.join(dirpath, name_foto)
                     date = time.gmtime(os.path.getmtime(all_dir_path))
                     time_ = list(date[:6])
-                    self.data.append([all_dir_path, time_, foto])
-        print(self.data)
-
-        sorted(self.data, key=lambda date_: date_[1][1])
-        sorted(self.data, key=lambda date_: date_[1][0])
+                    self.data.append([all_dir_path, time_, name_foto])
 
     def create(self, new_path):
-        self.new_path = new_path
         # создать пустой каталог (папку)
-        if not os.path.isdir(self.new_path):
-            os.mkdir(self.new_path)
+        self.new_path_main_dir = new_path
+        if not os.path.isdir(self.new_path_main_dir):
+            os.mkdir(self.new_path_main_dir)
 
-    def move(self):
+    def func_sort_file(self):
+        for old_path_photo, time_, name_foto in self.data:
+            new_name_year_dirs = os.path.join(self.new_path_main_dir,str(time_[0])+' Year')
+            new_name_month_dirs = os.path.join(new_name_year_dirs,str(time_[1])+' Month')
+            if not os.path.isdir(new_name_year_dirs):
+                os.makedirs(new_name_year_dirs)
+            if not os.path.isdir(new_name_month_dirs):
+                os.makedirs(new_name_month_dirs)
+            self.move_func_sort_file(new_name_month_dirs=new_name_month_dirs, name_foto=name_foto,
+                                     old_path_photo=old_path_photo)
+
+    def move_func_sort_file(self, new_name_month_dirs, name_foto, old_path_photo):
         # заменить (переместить) этот файл в другой каталог
-        for path in self.data:
-            text = os.path.join(self.new_path, path[2])
-            os.replace(path[0], text)
+        text = os.path.join(new_name_month_dirs, name_foto)
+        os.replace(old_path_photo, text)
 
 
 zip_name = 'icons.zip'
 A = FilesArrange(zip_name=zip_name, path='icons')
 A.unpacking_zip_file()
-A.normpath()
-A.walk_in_file()
-A.create(new_path='icons_by_year_1')
-A.move()
-
-
-class FilesArrangeHard(FilesArrange):
-    def walk_in_file(self):
-        last_name = None
-        for filenames in self.zfile.namelist():
-
-            if filenames and not filenames.endswith('/'):
-                foto = filenames[len(last_name):]
-                self.data.append([filenames, list(time.gmtime(os.path.getmtime(filenames))[0:6]), foto])
-            else:
-                last_name = filenames
-
-        sorted(self.data, key=lambda date_: date_[1][1])
-        sorted(self.data, key=lambda date_: date_[1][0])
-
-
-zip_name = 'icons.zip'
-A = FilesArrangeHard(zip_name=zip_name, path='icons')
-A.unpacking_zip_file()
-A.normpath()
-A.walk_in_file()
+A.walk_in_old_dir()
 A.create(new_path='icons_by_year')
-A.move()
-
-# TODO в обоих случаях работы сортировки изображений по подпапкам по годам не произошло -
-#  всё сохранилось в общей директории icons_by_year
+A.func_sort_file()
 
 # Усложненное задание (делать по желанию)
 # Нужно обрабатывать zip-файл, содержащий фотографии, без предварительного извлечения файлов в папку.
 # Это относится только к чтению файлов в архиве. В случае паттерна "Шаблонный метод" изменяется способ
 # получения данных (читаем os.walk() или zip.namelist и т.д.)
 # Документация по zipfile: API https://docs.python.org/3/library/zipfile.html
+
