310fd8f23fc3f3e7a91fd1675fc20211626c5081
Николай Хватов <Alset.Nikolas@yandex.ru>
2020-09-09 22:18:56 +0300

    14 модуль!

--- lesson_014/01_score.py
+++ lesson_014/01_score.py
@@ -32,8 +32,37 @@
 # Скрипт должен принимать параметр --result и печатать на консоль:
 #   Количество очков для результатов ХХХ - УУУ.
 
-# TODO тут ваш код
 
 # При написании кода помнить, что заказчик может захотеть доработок и новых возможностей...
 # И, возможно, вам пригодится паттерн проектирования "Состояние",
 #   см https://clck.ru/Fudd8 и https://refactoring.guru/ru/design-patterns/state
+import argparse
+
+from bowling import GetScore
+
+
+def show(game_result):
+    '''
+    Вывод на  экран
+    '''
+    print('----------------------')
+    len_i = 0
+    print('Ввод:')
+    for letter in game_result:
+
+        if letter == 'X':
+            print('X', end='')
+            len_i += 2
+        else:
+            print(letter, end='')
+            len_i += 1
+        if len_i % 2 == 0:
+            print(' ', end='')
+    print('\n----------------------')
+
+parser = argparse.ArgumentParser(description='Ping script')
+parser.add_argument('--result', action="store", dest="result")
+args = parser.parse_args()
+show(args.result)
+A = GetScore(args.result)
+print(f'"{args.result}" - {A.get_score()}' )
--- lesson_014/bowling.py
+++ lesson_014/bowling.py
@@ -1,98 +1,87 @@
-def show(game_result):
-    '''
-    Вывод на  экран
-    '''
-    print('----------------------')
-    len_i = 0
-    print('Ввод:')
-    for letter in game_result:
+class GetScore:
 
-        if letter == 'X':
-            print('X', end='')
-            len_i += 2
-        else:
-            print(letter, end='')
-            len_i += 1
-        if len_i % 2 == 0:
-            print(' ', end='')
-    print('\n----------------------')
+    def __init__(self, game_result, number_of_frames=4):
+        self.game_result = game_result
+        self.number_of_frames = number_of_frames
 
+    def check_len(self):
+        '''
+        Проверяем длину
+        '''
+        if self.game_result == '':
+            raise Exception('Не прошла проверку на корректность данных: длина!')
+        if self.game_result.count('X') + len(self.game_result) != 2 * self.number_of_frames:
+            print(self.game_result.count('X') + len(self.game_result))
 
-def check_len(game_result, number_of_frames):
-    '''
-    Проверяем длину
-    '''
-    if game_result.count('X') + len(game_result) != 2 * number_of_frames:
-        print(game_result.count('X') + len(game_result))
+            raise Exception('Не прошла проверку на корректность данных: не соответствует длина!')
 
-        raise Exception('Не прошла проверку на корректность данных: не соответствует длина!')
+    def check_edge_cases(self):
+        '''
+        Возможные проблемы с /
+        '''
+        len_i = 0
+        for letter in self.game_result:
+            if letter == 'X':
+                len_i += 2
+            else:
+                len_i += 1
+            if len_i % 2 == 1 and letter == '/':
+                raise Exception('Не прошла проверку на корректность данных: inverted spare  !')
 
+    def check_summa_nearby(self):
+        '''
+            Возможные проблемы с суммой в каждом из фреймов
+        '''
+        i = 0
+        while i < len(self.game_result) - 1:
 
-def check_edge_cases(game_result):
-    '''
-    Возможные проблемы с /
-    '''
-    len_i = 0
-    for letter in game_result:
-        if letter == 'X':
-            len_i += 2
-        else:
-            len_i += 1
-        if len_i % 2 == 1 and letter == '/':
-            raise Exception('Не прошла проверку на корректность данных: inverted spare  !')
-
-
-def check_summa_nearby(game_result):
-    '''
-        Возможные проблемы с суммой в каждом из фреймов
-    '''
-    i = 0
-    while i < len(game_result) - 1:
-
-        if game_result[i].isdigit() and game_result[i + 1].isdigit():
-            if int(game_result[i]) + int(game_result[i + 1]) > 10:
-                raise Exception(f'Не прошла проверку на корректность данных: mistake summa  '
-                                f'{game_result[i]}+{game_result[i + 1]}>10 !')
+            if self.game_result[i].isdigit() and self.game_result[i + 1].isdigit():
+                if int(self.game_result[i]) + int(self.game_result[i + 1]) > 10:
+                    raise Exception(f'Не прошла проверку на корректность данных: mistake summa  '
+                                    f'{self.game_result[i]}+{self.game_result[i + 1]}>10 !')
+                i += 1
             i += 1
-        i += 1
+
+    def check_all_symbols(self):
+        all_symbols = {'X', "/", '-'} | {str(x) for x in range(10)}
+        for letter in self.game_result:
+            if letter not in all_symbols:
+                raise Exception(f'Не прошла проверку на корректность данных: mistake letter', letter)
+
+    def check_rus_x(self):
+        if 'х' in self.game_result or 'Х' in self.game_result:
+            raise Exception(f'ввод Х на русском! Требуется X EN')
+
+    def check_param(self):
+        '''
+        Проверка на корректность данных  Например:«Х4/34-4»
+        '''
+        self.check_rus_x()
+        self.check_len()
+        self.check_all_symbols()
+        self.check_edge_cases()
+        self.check_summa_nearby()
+
+    def get_score(self):
+        self.check_param()
+        result = 0
+        i = 0
+        while i < len(self.game_result) - 1:
+            if self.game_result[i] == 'X':
+                result += 20
+                i -= 1
+            elif self.game_result[i].isdigit() and self.game_result[i + 1].isdigit():
+                result += int(self.game_result[i]) + int(self.game_result[i + 1])
+            elif self.game_result[i + 1] == '/':
+                result += 15
+            elif self.game_result[i] == '-' and self.game_result[i + 1].isdigit():
+                result += int(self.game_result[i + 1])
+            elif self.game_result[i + 1] == '-' and self.game_result[i].isdigit():
+                result += int(self.game_result[i])
+            i += 2
+        return result
 
 
-def check_all_symbols(game_result):
-    all_symbols = {'X', "/", '-'} | {str(x) for x in range(10)}
-    for letter in game_result:
-        if letter not in all_symbols:
-            raise Exception(f'Не прошла проверку на корректность данных: mistake letter', letter)
-
-def check_param(game_result):
-    '''
-    Проверка на корректность данных  Например:«Х4/34-4»
-    '''
-    number_of_frames = 4
-    check_len(game_result, number_of_frames)
-    check_all_symbols(game_result)
-    check_edge_cases(game_result)
-    check_summa_nearby(game_result)
-
-
-
-def get_score(game_result):
-    show(game_result)
-    check_param(game_result)
-    result = 0
-    i = 0
-    while i < len(game_result) - 1:
-        if game_result[i] =='X':
-            result +=20
-            i-=1
-        elif game_result[i].isdigit() and game_result[i + 1].isdigit():
-            result += int(game_result[i]) + int(game_result[i + 1])
-        elif game_result[i + 1] == '/':
-            result +=15
-        elif game_result[i] == '-' and game_result[i + 1].isdigit():
-            result +=int(game_result[i + 1])
-        elif game_result[i+1] == '-' and game_result[i].isdigit():
-            result += int(game_result[i])
-        i+=2
-    return result
-
-get_score('X4/14-4')
+if __name__ == '__main__':
+    A = GetScore('X4/14-4')
+    print(A.get_score())
