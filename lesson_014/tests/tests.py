7a152b0ad6cbd3eb3cd686de4fe6ee97f4f4e2d1
Максим <bi4ok23@gmail.com>
2020-09-11 20:47:45 +0300

    lesson 014: нужны правки в 01

--- lesson_014/tests/tests.py
+++ lesson_014/tests/tests.py
@@ -1,9 +1,8 @@
 import unittest
 from bowling import GetScore
 
+
 class ExternalResourceGetterTest(unittest.TestCase):
-
-
 
     def bug_catcher(self, result):
         with self.assertRaises(Exception) as cm:
@@ -13,7 +12,7 @@ class ExternalResourceGetterTest(unittest.TestCase):
 
     def test_check_len_long(self):
         some_exception = Exception('Не прошла проверку на корректность данных: не соответствует длина!')
-        the_exception =self.bug_catcher('X'*20)
+        the_exception = self.bug_catcher('X' * 20)
         self.assertEqual(the_exception, str(some_exception))
 
     def test_check_len_short(self):
@@ -23,43 +22,38 @@ class ExternalResourceGetterTest(unittest.TestCase):
 
     def test_edge_cases_v1(self):
         some_exception = Exception('Не прошла проверку на корректность данных: inverted spare  !')
-        the_exception = self.bug_catcher('X//' + '1'*16)
+        the_exception = self.bug_catcher('X//' + '1' * 16)
         self.assertEqual(the_exception, str(some_exception))
-
 
     def test_edge_cases_v2(self):
         some_exception = Exception('Не прошла проверку на корректность данных: inverted spare  !')
-        the_exception = self.bug_catcher('X/' + '1'*17)
+        the_exception = self.bug_catcher('X/' + '1' * 17)
         self.assertEqual(the_exception, str(some_exception))
-
 
     def test_summa_nearby_v1(self):
         some_exception = Exception(f'Не прошла проверку на корректность данных: mistake summa !')
-        the_exception = self.bug_catcher('X9' + '1'*17)
+        the_exception = self.bug_catcher('X9' + '1' * 17)
         self.assertEqual(the_exception, str(some_exception))
-
 
     def test_all_symbols_v1(self):
         some_exception = Exception(f'Не прошла проверку на корректность данных: mistake letter')
-        the_exception = self.bug_catcher('X0' + '1'*17)
+        the_exception = self.bug_catcher('X0' + '1' * 17)
         self.assertEqual(the_exception, str(some_exception))
 
     def test_all_symbols_v2(self):
         some_exception = Exception(f'Не прошла проверку на корректность данных: mistake letter')
-        the_exception = self.bug_catcher('X*' + '1'*17)
+        the_exception = self.bug_catcher('X*' + '1' * 17)
         self.assertEqual(the_exception, str(some_exception))
 
     def test_all_symbols_v3(self):
         some_exception = Exception(f'Не прошла проверку на корректность данных: mistake letter')
-        the_exception = self.bug_catcher('X!' + '1'*17)
+        the_exception = self.bug_catcher('X!' + '1' * 17)
         self.assertEqual(the_exception, str(some_exception))
-
 
     def test_rus_x(self):
         some_exception = Exception(f'ввод Х на русском! Требуется X EN')
-        the_exception = self.bug_catcher('Х!' + '1'*17)
+        the_exception = self.bug_catcher('Х!' + '1' * 17)
         self.assertEqual(the_exception, str(some_exception))
-
 
 
 if __name__ == '__main__':
