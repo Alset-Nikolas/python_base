fb9743e4d95209151af18297de8db0322d76da14
Николай Хватов <Alset.Nikolas@yandex.ru>
2020-09-23 22:47:11 +0300

    16 модуль распарсил сайт

--- lesson_016/01_weather.py
+++ lesson_016/01_weather.py
@@ -13,6 +13,35 @@
 # Создать модуль-движок с классом WeatherMaker, необходимым для получения и формирования предсказаний.
 # В нём должен быть метод, получающий прогноз с выбранного вами сайта (парсинг + re) за некоторый диапазон дат,
 # а затем, получив данные, сформировать их в словарь {погода: Облачная, температура: 10, дата:datetime...}
+from pprint import pprint
+
+import requests
+from bs4 import BeautifulSoup
+# сегодня
+html_text = requests.get('https://www.meteoservice.ru/weather/now/moskva').text
+soup = BeautifulSoup(html_text, 'html.parser')
+date = soup.find(id='point-time').contents[0]
+temperature = soup.find('span', class_='value').contents[0]
+a = soup.find('p', class_='margin-bottom-0').contents[0]
+print(date)
+print(temperature)
+print(a)
+print()
+#на 10 дней
+html_text_10 = requests.get('https://www.meteoservice.ru/weather/10days/moskva').text
+soup_1 = BeautifulSoup(html_text_10, 'html.parser')
+date_ = soup_1.find_all(class_="text-nowrap grey font-condensed font-smaller")
+temperature_ = soup_1.find_all('div', class_="font-larger")
+a_ = soup_1.find_all('div', class_="column value show-for-large text-left font-smaller")
+for i in range(1,10):
+
+    print(date_[i].contents[0])
+    print(temperature_[i].contents[0].split()[0])
+    print(a_[i].contents[0])
+    print()
+
+class W
+
 
 # Добавить класс ImageMaker.
 # Снабдить его методом рисования открытки
