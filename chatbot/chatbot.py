281da4eb1cc5bb593100b6fde3ef714bdfddcd3f
Максим <bi4ok23@gmail.com>
2020-10-07 18:15:38 +0300

    lesson 016: работаем над ботом

--- chatbot/chatbot.py
+++ chatbot/chatbot.py
@@ -5,16 +5,15 @@ from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
 import logging
 
 log = logging.getLogger('bot')
+
+
 def configure_logging():
-
-
     stream_handler = logging.StreamHandler()
     stream_handler.setFormatter(logging.Formatter("%(levelname)s %(message)s"))
     stream_handler.setLevel(logging.INFO)
     log.addHandler(stream_handler)
 
-
-    file_handler=logging.FileHandler("bot.log", encoding='UTF-8', mode='w')
+    file_handler = logging.FileHandler("bot.log", encoding='UTF-8', mode='w')
     file_handler.setFormatter(logging.Formatter("%(asctime)s %(levelname)s %(message)s"))
     file_handler.setLevel(logging.DEBUG)
     log.addHandler(file_handler)
@@ -27,6 +26,7 @@ try:
 except ImportError as er:
     print(er)
     exit("DO cp settings.py.default settings.py and set token!")
+
 
 class Bot:
     '''
@@ -46,15 +46,15 @@ class Bot:
         self.long_poller = VkBotLongPoll(vk=self.vk, group_id=self.group_id)
         self.api = self.vk.get_api()
 
-
     def run(self):
         '''Запуск бота.'''
         for event in self.long_poller.listen():
             log.debug("полученло событие")
             try:
                 self.on_event(event)
-            except Exception :
-                log.exception('Ошибка в обработке события:'  )
+            except Exception:
+                log.exception('Ошибка в обработке события:')
+
     def on_event(self, event):
         '''Отправляет сообщение назад, если это текст.
         :param event: VkBotMessageEvent
@@ -63,7 +63,7 @@ class Bot:
             log.debug("отправляем сообщение назад")
             self.api.messages.send(
                 message=event.object['message']['text'],
-                random_id=randint(0, 2**20),
+                random_id=randint(0, 2 ** 20),
                 peer_id=event.object['message']['peer_id']
             )
         else:
--- chatbot/tests.py
+++ chatbot/tests.py
@@ -9,32 +9,34 @@ from bot import Bot
 
 
 class Test1(TestCase):
-
-    RAW_EVENT ={
+    RAW_EVENT = {
         'type': 'message_new',
         'object': {'message': {'date': 1600102810, 'from_id': 548199338, 'id': 89, 'out': 0,
-                              'peer_id': 548199338, 'text': 'м', 'conversation_message_id': 88,
-                              'fwd_messages': [], 'important': False, 'random_id': 0, 'attachments': [],
-                              'is_hidden': False},
-                  'client_info': {'button_actions': ['text', 'vkpay', 'open_app', 'location', 'open_link'],
-                                  'keyboard': True, 'inline_keyboard': True, 'carousel': False, 'lang_id': 0}},
+                               'peer_id': 548199338, 'text': 'м', 'conversation_message_id': 88,
+                               'fwd_messages': [], 'important': False, 'random_id': 0, 'attachments': [],
+                               'is_hidden': False},
+                   'client_info': {'button_actions': ['text', 'vkpay', 'open_app', 'location', 'open_link'],
+                                   'keyboard': True, 'inline_keyboard': True, 'carousel': False, 'lang_id': 0}},
         'group_id': 198507410, 'event_id': '7eaa4ff72c79d802328422c91cde763d6ee5b4a5'}
 
     def test_run(self):
         count = 5
-        events= [{}] * count
+        events = [{}] * count
         long_poller_mock = Mock(return_value=events)
         long_poller_listen_mock = Mock()
         long_poller_listen_mock.listen = long_poller_mock
-        with patch("VkApi"):
-            with patch("VkBotLongPoll",
-                       return_value=long_poller_listen_mock
-                       ):
+        with patch("VkApi"):  # TODO в патче нужно указать что именно вы хотите заменить
+            # TODO если просто указать VkApi - то он будет пытаться заменить этот VkApi тут
+            # TODO а нам надо заменить его в модуле chatbot (или bot) при этом не просто VkApi
+            # TODO а vk_api.VkApi, т.к. мы не импортируем его напрямую, а используем из этой библиотеки
+            # TODO ниже в комментах у вас правильные пути добавлены, насколько я вижу
+            with patch("VkBotLongPoll", return_value=long_poller_listen_mock):
                 bot = Bot("", "")
                 bot.on_event = Mock()
                 bot.run()
                 bot.on_event.assert_called_with({})
                 assert bot.on_event.call_count == count
+
     '''
     def test_on_event(self):
         event = VkBotMessageEvent(raw=self.RAW_EVENT)
@@ -74,24 +76,34 @@ class Test1(TestCase):
         send_mock = Mock()
         api_mock = Mock()
         api_mock.message.send = send_mock
+        # TODO в боте у вас есть метод self.api.messages.send
+        # TODO если вы хотите заменить его, то надо указать не .message.send а .messages.send
 
         events = []
         for input_text in self.INPUTS:
             event = deepcopy(self.RAW_EVENT)
             event["object"]["text"] = input_text
-            events.append(VkBotEvent(event))
+            events.append(VkBotEvent(event))  # TODO тут нужен VkBotMessageEvent например
+            # TODO from vk_api.bot_longpoll import VkBotMessageEvent, VkBotEvent -- сперва его стоит импортировать
 
         long_poller_mock = Mock()
         long_poller_mock.listen = Mock(return_value=events)
 
         with patch("bot.VkBotLongPoll", return_value=long_poller_mock):
-            bot = Bot("","")
+            bot = Bot("", "")
             bot.api = api_mock
             bot.run()
-        assert  send_mock.call_count == len(self.INPUTS)
+        assert send_mock.call_count == len(self.INPUTS)
 
         real_outputs = []
         for call in send_mock.call_args_list:
             args, kwargs = call
             real_outputs.append(kwargs["mwssage"])
+        for real, expec in zip(real_outputs, self.EXPECTED_OUTPUTS):
+            print(real)
+            print('-' * 50)
+            print(expec)
+            print('-' * 50)
+            print(real == expec)
+            print('_' * 50)
         assert real_outputs == self.EXPECTED_OUTPUTS
